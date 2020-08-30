05eae0a5d72d135070fe42e8a71c20897f77f446
Paula Grangeiro <paula.grangeiro@thoughtworks.com>
2020-08-30 18:58:17 -0300

    checkpoint

--- src/youtube/services.py
+++ src/youtube/services.py
@@ -1,18 +1,17 @@
 from pyyoutube import Api
 
+from src.youtube.clients import PlaylistClient
 from src.youtube.factories import VideoFactory
 
 
 def search_video(api_key, search_keywords):
 	api = Api(api_key=api_key)
 	data = api.search_by_keywords(q=search_keywords, limit=1, search_type='video')
-	
-	return VideoFactory.create(data)
+	return VideoFactory.create(data.to_dict()['items'])
 
 
-def create_playlist(token):
-	pass
-
-
-def add_video_in_playlist(playlist_id, video_id, token):
-	pass
+def create_playlist(token, playlist_name, videos):
+	client = PlaylistClient(token)
+	playlist_id = client.create(playlist_name)['id']
+	for item in videos:
+		client.add_video(playlist_id, item.id)

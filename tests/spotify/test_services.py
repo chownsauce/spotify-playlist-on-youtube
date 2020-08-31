24d9d09f370611a57fadf855620973d0b862398f
Paula Grangeiro <paula.grangeiro@thoughtworks.com>
2020-08-31 11:40:14 -0300

    Remove pyyoutube dependency

--- tests/spotify/test_services.py
+++ tests/spotify/test_services.py
@@ -3,11 +3,15 @@ from unittest.mock import call, patch
 from src.spotify.services import get_playlist
 
 
+@patch('src.spotify.services.PlaylistFactory')
 @patch('src.spotify.services.TrackFactory')
 @patch('src.spotify.services.PlaylistClient')
 @patch('src.spotify.services.TokenClient')
-def test_get_playlist_successfully(mocked_token_client, mocked_playlist_client, mocked_factory):
-	expected = {'Liquorice': 'Tell me if you like your lady in my-my color. Can I be your type? yeah'}
+def test_get_playlist_successfully(mocked_token_client, mocked_playlist_client, mocked_track_factory, mocked_playlist_factory):
+	expected = {
+		'name': 'Playlist',
+		'items': [{'Liquorice': 'Tell me if you like your lady in my-my color. Can I be your type? yeah'}]
+	}
 	token_response = {'access_token': 'token'}
 	playlist_response = {
 		'tracks': {
@@ -18,24 +22,31 @@ def test_get_playlist_successfully(mocked_token_client, mocked_playlist_client, 
 
 	mocked_token_client().post.return_value = token_response
 	mocked_playlist_client().get.return_value = playlist_response
-	mocked_factory.create.return_value = expected
+	mocked_track_factory.create.return_value = expected['items'][0]
+	mocked_playlist_factory.create.return_value = expected
 
 	playlist = get_playlist('id', 'secret', 'playlist')
 
-	assert playlist == [expected]
+	assert playlist == expected
 	mocked_token_client.assert_called_with('id', 'secret')
 	mocked_playlist_client().get.assert_called_once_with('playlist', 'token')
-	mocked_factory.create.assert_called_once_with(playlist_response['tracks']['items'][0]['track'])
+	mocked_track_factory.create.assert_called_once_with(playlist_response['tracks']['items'][0]['track'])
+	mocked_playlist_factory.create.assert_called_once_with(playlist_response, expected['items'])
 
 
+@patch('src.spotify.services.PlaylistFactory')
 @patch('src.spotify.services.TrackFactory')
 @patch('src.spotify.services.PlaylistClient')
 @patch('src.spotify.services.TokenClient')
-def test_get_playlist_with_next_page_successfully(mocked_token_client, mocked_playlist_client, mocked_factory):
-	expected = [
+def test_get_playlist_with_next_page_successfully(mocked_token_client, mocked_playlist_client, mocked_track_factory, mocked_playlist_factory):
+	expected_tracks = [
 		{'Liquorice': 'Tell me if you like your lady in my-my color. Can I be your type? yeah'},
 		{'Hello Bitches': '뛰자뛰자 방방방 디스코 팡팡팡 hello Bitcheeeees'}
 	]
+	expected = {
+		'name': 'Playlist',
+		'items': list(expected_tracks)
+	}
 	token_response = {'access_token': 'token'}
 	playlist_response = [{
 		'tracks': {
@@ -50,14 +61,16 @@ def test_get_playlist_with_next_page_successfully(mocked_token_client, mocked_pl
 	mocked_token_client().post.return_value = token_response
 	mocked_playlist_client().get.return_value = playlist_response[0]
 	mocked_playlist_client().next.return_value = playlist_response[1]
-	mocked_factory.create.side_effect = expected
+	mocked_track_factory.create.side_effect = expected_tracks
+	mocked_playlist_factory.create.return_value = expected
 
 	playlist = get_playlist('id', 'secret', 'playlist')
 
 	assert playlist == expected
 	mocked_token_client.assert_called_with('id', 'secret')
 	mocked_playlist_client().get.assert_called_once_with('playlist', 'token')
-	mocked_factory.create.assert_has_calls([
+	mocked_track_factory.create.assert_has_calls([
 		call(playlist_response[0]['tracks']['items'][0]['track']), 
 		call(playlist_response[1]['items'][0]['track'])
 	])
+	mocked_playlist_factory.create.assert_called_once_with(playlist_response[0], expected_tracks)
